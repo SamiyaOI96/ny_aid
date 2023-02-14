f92b499a603ae4bd91059ea91541cc837305ffa5
samiya <samiyai96@gmail.com>
2023-02-14 10:59:06 -0500

    commmiting again.

--- ny_aid/settings.py
+++ ny_aid/settings.py
@@ -129,11 +129,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_heroku
+# import django_heroku
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
-options = DATABASES['default'].get('OPTIONS', {})
-options.pop('sslmode', None)
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# options = DATABASES['default'].get('OPTIONS', {})
+# options.pop('sslmode', None)
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
